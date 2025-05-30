3e9e14273cdb321ca6cd99ed16d1df945d2f210c
Ryan Alves Da Cunha Costa <ryancunhacostaa@gmail.com>
2025-05-30 12:49:58 -0300

    Aula sobre boolean

---
+++ condicionais/condicionais.java
@@ -0,0 +1,99 @@
+package condicionais;
+
+public class condicionais {
+    public static void main(String[] args) {
+        //O queé boolean??
+        /**
+         * Boolean é um tipo de dado primitivo em java que pode armazenar apenas dois 
+         * valores: TRUE ou FALSE;
+         * 
+         * Utilizado para controlar fluxo de execução em estruturas de controle como IF, WHILE e FOR;
+         * 
+         * Suporta operadores lógicos como &&(AND), ||(OR), e !(NOT) para combinar e inventar valores booleanos;
+         * 
+         * Uma expressão booleana é qualquer expressão que resulta em valor true ou false;
+         * 
+         * Emora seja representado como true e false no código, é tratado como 1(true) ou 0(false);
+         */
+
+
+        boolean  isTrue = true;
+        boolean isFalse = false;
+
+        //boolean aceita somente TRUE ou FALSE 
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+
+
+        //Operadores de comparação
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
