e1582ab4381bdc387b75ca6287f29539c8cc1299
Ryan Alves Da Cunha Costa <ryancunhacostaa@gmail.com>
2025-06-06 17:03:12 -0300

    Condição (&&)

--- condicionais/operadoresogicos.java
+++ condicionais/operadoresogicos.java
@@ -22,58 +22,40 @@ public class operadoresogicos {
              //Uso comum: Combinação de múltiplas condições que precisam ser verdadeiras ao mesmo tempo;
              // Retorna true e ambas as condições forem verdadeiras;
 
+                int idade = 17;
+                boolean temCNH = true;
+
+                System.out.println(idade >= 18 && temCNH);
+
+                boolean carteiraVencida = true;
+
+                System.out.println(idade >= 18 && temCNH && carteiraVencida == false);
+                 
+                int A = 10;
+                int B = 20;
+                    //True && True => True
+                if (A > 5 && B > 10) {
+                    System.out.println("Deu certo!");
+                }
+                    
+                    //False && Treu => True (Curto circuito)
+                if (A > 55 & B > 10) {
+                    System.out.println("Deu certo!");
+                }
 
 
+                //Operador OR
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-             
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                /**
+                 * O operador || (OR), retorna true pelo menos uma das condições dor verdadeiras;
+                 * 
+                 * Sintaxe: Condição1 || Condição2;
+                 * 
+                 * Curto circuito: Se a primeira condição dor true, a segunda condição não é avaliada;
+                 * 
+                 * Uso comum: Verificaçãp de múltiplas condiões onde apenas uma precisa ser verdadeira;
+                 * 
+                 */
 
 
 
@@ -88,5 +70,50 @@ public class operadoresogicos {
 
 
              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }    
 }
