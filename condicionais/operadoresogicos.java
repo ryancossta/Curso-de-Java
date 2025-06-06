e0a5829bfcd4f08b2724cbd013c33026a115e14e
Ryan Alves Da Cunha Costa <ryancunhacostaa@gmail.com>
2025-06-06 16:50:48 -0300

    Operadores lógicos

---
+++ condicionais/operadoresogicos.java
@@ -0,0 +1,92 @@
+package condicionais;
+
+public class operadoresogicos {
+    public static void main(String[] args) {
+         // Aula sobre - OPERADORES LÓGICOS 
+
+            /**
+             * && (E lógico): Retorna true se amn=bas as condições forem verdadeiras;
+             * 
+             * || (OU lógico): Retorna true se pelo menos uma das condições for verdadeiras;
+             * 
+             * ! (NÃO lógico): Inverte o valor lógico; retorna true se a condição dor falsa e vice-versa;
+             * 
+             * Combinação: Pode combinar múltiplas condições em uma única expressão lógica;
+             */
+
+
+             // Trabalhando com AND (&&)
+
+             //Sintaxe: Condição1 && Condição2;
+             //Sea primiera condição for false, a segunda condição não é avaliada;
+             //Uso comum: Combinação de múltiplas condições que precisam ser verdadeiras ao mesmo tempo;
+             // Retorna true e ambas as condições forem verdadeiras;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
+    }    
+}
